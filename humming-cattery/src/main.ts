c525319a82defcb3a0703b10aa4f192a240b4a19
Tomi <ot_tomi@hotmail.com>
2024-02-09 01:35:37 +0200

    Update src/main.ts

--- humming-cattery/src/main.ts
+++ humming-cattery/src/main.ts
@@ -1,8 +1,11 @@
 import './assets/main.css'
 import { createApp } from 'vue'
 import './style.css'
+// @ts-ignore
 import App from './App.vue'
+// @ts-ignore
 import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
+// @ts-ignore
 import router from './router'
 
 createApp(App).use(bootstrap).use(router).mount('#app')
